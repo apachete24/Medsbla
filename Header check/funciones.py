d42bffa6f2f60cf95f641b30841939d9fba6ac58
dasonodas <49148040+dasonodas@users.noreply.github.com>
2020-05-19 11:29:04 +0200

    Update funciones.py

--- Header check/funciones.py
+++ Header check/funciones.py
@@ -2,54 +2,75 @@ from termcolor import colored
 
 
 def evaluate_warn (headers):
-    warn = 0
+    warn: int = 0
+
+
     try:
-
-
-        if(headers['x-frame-options'] != "SAMEORIGIN"):
+        if (headers['x-frame-options'] != "SAMEORIGIN"):
             warn += 1
             output = colored("WARNING\n Incorrect value for <x-frame-option> header", 'red')
             print(output)
-
-        try:
-            headers['strict-transport-security']
-        except:
-            warn += 1
-            output = colored("WARNING\n Incorrect value for <strict-transport-security>", 'red')
-            print(output)
-
-        try:
-            headers['access-control-allow-origin']
-        except:
-            warn +=1
-            output = colored("WARNING\n Incorrect value for <access-control-allow-origin>", 'red')
-            print(output)
+        else:
+            print(colored("\n<x-frame-option>", 'green'))
+    except:
+        warn += 1
+        print(colored("WARNING\n <x-frame-option> header is empty", 'red'))
 
 
-        if (headers['content-security-policy'] == '*'):
-            warn += 1
-            output = colored("WARNING\n Incorrect value for <content-security-policy>", 'red')
-            print(output)
+    try:
+        if (headers['strict-transport-security']):
+            print(colored("\nstrict-transport-security", "green"))
+    except:
+        warn += 1
+        print(colored("WARNING\n Incorrect value for <strict-transport-security>", 'red'))
 
-        if headers['x-xss-protection']:
-            if headers['x-xss-protection'].lower() not in ['1', '1; mode=block']:
+
+    try:
+        if (headers['access-control-allow-origin']):
+            print(colored("\naccess-control-allow-origin", "green"))
+    except:
+        warn += 1
+        print(colored("WARNING\n Incorrect value for <access-control-allow-origin>", 'red'))
+
+
+    try:
+        if (headers['content-security-policy']):
+            print(colored("content-security-policy", 'green'))
+    except:
+        warn += 1
+        print(colored("WARNING\n Incorrect value for <content-security-policy>", 'red'))
+
+
+    try:
+        if (headers['x-xss-protection']):
+            if headers['x-xss-protection'].lower() in ['1', '1; mode=block']:
+                print(colored("x-xss-protection", 'green'))
+            else:
                 warn += 1
-                output = colored("WARNING\n Incorrect value for <x-xss-protection>", 'red')
-                print((output))
+                print(colored("WARNING\n Incorrect value for <x-xss-protection>", 'red'))
+    except:
+        warn += 1
+        print(colored("WARNING\n Incorrect value for <x-xss-protection>", 'red'))
 
-        if header['x-content-type-options'].lower() != 'nosniff':
-                warn += 1
-                output = colored("WARNING\n Incorrect value for <x-content-type-options>", 'red')
-                print((output))
 
+    try:
+        if headers['x-content-type-options'].lower() == 'nosniff':
+            print(colored("x-content-type-options", 'green'))
+        else:
+            warn += 1
+            print(colored("WARNING\n Incorrect value for <x-content-type-options>", 'red'))
+    except:
+        warn += 1
+        print(colored("WARNING\n Incorrect value for <x-content-type-options>", 'red'))
+
+    try:
         if headers['x-powered-by']['server']:
             warn += 1
-            output = colored("WARNING\n Server version is disclosed", 'red')
-            print((output))
-
+            print(colored("WARNING\n Server version is disclosed", 'red'))
     except:
-        if (warn > 0):
-            print(colored("Execution ended with some errors", 'red'))
+        print(colored("x-powered-by", 'green'))
 
-    finally:
-        return warn
+    if (warn > 0):
+        print(colored("Execution ended with some errors", 'red'))
+
+    return warn
